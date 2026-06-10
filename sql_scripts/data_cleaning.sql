9973509b35a7cfe0152cd115a09a0323d0b99f1b
Abdul-Wasey-M <mdabdulwa3@gmail.com>
2026-06-10 23:44:23 +0530

    Handled missing and null values in industry field

--- sql_scripts/data_cleaning.sql
+++ sql_scripts/data_cleaning.sql
@@ -130,6 +130,45 @@ SELECT *
 FROM layoffs_staging2;
 
 -- Step 4: Handle null values
+SELECT *
+FROM layoffs_staging2;
+
+SELECT *
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL;
+
+SELECT *
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL;
+
+UPDATE layoffs_staging2
+SET industry=NULL 
+WHERE industry = '';
+
+SELECT *
+FROM layoffs_staging2
+WHERE industry IS NULL OR industry='';
+
+SELECT *
+FROM layoffs_staging2
+WHERE company LIKE 'Carvana';
+
+SELECT * 
+FROM layoffs_staging2 t1
+JOIN layoffs_staging2 t2
+ON t1.company = t2.company
+WHERE t1.industry IS NULL AND t2.industry IS NOT NULL;
+
+UPDATE layoffs_staging2 t1
+JOIN layoffs_staging2 t2
+ON t1.company = t2.company
+SET t1.industry=t2.industry
+WHERE t1.industry IS NULL  AND t2.industry IS NOT NULL;
+
+
+
+
 
 -- Step 5: Remove unnecessary records
 
